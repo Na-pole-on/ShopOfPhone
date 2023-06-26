da89af50d442674513b0a67398dda57faa3020a1
Na-pole-on <juikomathew@gmail.com>
2023-06-26 22:17:19 +0300

    Add project files.

---
+++ ShopOfPhone/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ShopOfPhone.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
