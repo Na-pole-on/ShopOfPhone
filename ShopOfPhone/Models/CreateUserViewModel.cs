1bf9945a89a17cc95ea143876ffcc5d531336d65
Na-pole-on <juikomathew@gmail.com>
2023-07-06 16:01:53 +0300

    end disign of sign up

--- ShopOfPhone/Models/CreateUserViewModel.cs
+++ ShopOfPhone/Models/CreateUserViewModel.cs
@@ -1,8 +1,18 @@
-﻿namespace ShopOfPhone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopOfPhone.Models
 {
     public class CreateUserViewModel
     {
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
+
+        [Required]
         public string? Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string? RepeatPassword { get; set; }
     }
 }
